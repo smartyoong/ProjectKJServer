eee6448a7643ee1a4286dca8d6a482f889a6e826
Kim Young Chan <smartyoong@naver.com>
2024-05-03 23:18:51 +0900

    where 제약조건절 개선을 통해 PacketUtil 수정 관련 함수들 전부 수정 필요

--- ProjectKJServers/Utility/PacketUtility.cs
+++ ProjectKJServers/Utility/PacketUtility.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Globalization;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text.Json;
 
 namespace KYCPacket
@@ -21,16 +24,7 @@ namespace KYCPacket
             return JsonSerializer.SerializeToUtf8Bytes(Data);
         }
 
-        public static int IDToInt<E>(E ID) where E : Enum
-        {
-            if (!(ID is IConvertible))
-            {
-                throw new ArgumentException("ID 값은 반드시 int와 호환되어야합니다.");
-            }
-            return Convert.ToInt32(ID);
-        }
-
-        public static byte[] MakePacketFromStruct<S>(int ID, S Packet) where S : struct
+        public static byte[] MakePacket<E,S>(E ID, S Packet) where S : struct where E : Enum, IConvertible
         {
             // 사이즈를 구한다.
             int PacketSize = Marshal.SizeOf(Packet);
@@ -38,7 +32,7 @@ namespace KYCPacket
 
             // 각 데이터를 직렬화한다.
             var SizeBuffer = BitConverter.GetBytes(TotalSize);
-            var IDBuffer = BitConverter.GetBytes(ID);
+            var IDBuffer = BitConverter.GetBytes(ID.ToInt32(CultureInfo.CurrentCulture));
             var PacketBuffer = SerializePacket(Packet);
 
             // 복사 비용을 줄이기 위해 Memory를 사용한다.
@@ -68,29 +62,54 @@ namespace KYCPacket
             return ReturnSpan.ToArray();
         }
 
-        public static Memory<byte> ByteToMemory(ref byte[] Packet)
-        {
-            return Packet.AsMemory();
-        }
+        //public static Memory<byte> ByteToMemory(ref byte[] Packet)
+        //{
+        //    return Packet.AsMemory();
+        //}
 
-        public static EResult GetIDFromPacket<EResult>(ref Memory<byte> Packet) where EResult : Enum
+        //public static EResult GetIDFromPacket<EResult>(ref Memory<byte> Packet) where EResult : Enum
+        //{
+        //    // ID를 가져온다.
+        //    int ID = BitConverter.ToInt32(Packet.Span);
+        //    // 검증한다.
+        //    bool IsEnumDefined = Enum.IsDefined(typeof(EResult), ID);
+        //    if (!IsEnumDefined)
+        //    {
+        //        throw new ArgumentException("ID 값이 Enum에 정의되어 있지 않습니다.");
+        //    }
+        //    // 나머지 데이터만 남겨둔다
+        //    Packet = Packet.Slice(sizeof(int));
+        //    return (EResult)Enum.ToObject(typeof(EResult), ID);
+        //}
+
+        //public static SResult? GetPacketStruct<SResult>(ref Memory<byte> Packet) where SResult : struct
+        //{
+        //    return DeserializePacket<SResult>(ref Packet);
+        //}
+
+        public static (EResult, SResult) GetPacket<EResult,SResult>(ref byte[] Data) where EResult : Enum, IConvertible where SResult : struct
         {
+            // Memory로 변환한다.
+            Memory<byte> MemoryData = Data.AsMemory();
             // ID를 가져온다.
-            int ID = BitConverter.ToInt32(Packet.Span);
+            int IDValue = BitConverter.ToInt32(MemoryData.Span);
             // 검증한다.
-            bool IsEnumDefined = Enum.IsDefined(typeof(EResult), ID);
+            bool IsEnumDefined = Enum.IsDefined(typeof(EResult), IDValue);
             if (!IsEnumDefined)
             {
                 throw new ArgumentException("ID 값이 Enum에 정의되어 있지 않습니다.");
             }
+            // enum 제약 조건이 없기에 어쩔 수 없는 박싱, 언박싱
+            EResult ID = (EResult)Enum.ToObject(typeof(EResult), IDValue);
             // 나머지 데이터만 남겨둔다
-            Packet = Packet.Slice(sizeof(int));
-            return (EResult)Enum.ToObject(typeof(EResult), ID);
-        }
-
-        public static SResult? GetPacketStruct<SResult>(ref Memory<byte> Packet) where SResult : struct
-        {
-            return DeserializePacket<SResult>(ref Packet);
+            MemoryData = MemoryData.Slice(sizeof(int));
+            // 데이터를 가져온다.
+            SResult? Result = DeserializePacket<SResult>(ref MemoryData);
+            if (Result == null)
+            {
+                throw new ArgumentException("패킷 데이터를 가져오는데 실패했습니다.");
+            }
+            return (ID, Result.Value);
         }
     }
 }
