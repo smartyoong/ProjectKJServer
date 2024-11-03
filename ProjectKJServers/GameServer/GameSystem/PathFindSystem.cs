1468c9839bff2240a12fe45dd3993de59f35f8d6
Kim Young Chan <smartyoong@naver.com>
2024-11-03 19:47:54 +0900

    일부 구조체 인터페이스 수정

---
+++ ProjectKJServers/GameServer/GameSystem/PathFindSystem.cs
@@ -0,0 +1,44 @@
+﻿using GameServer.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.GameSystem
+{
+    public interface IHuristic
+    {
+        public float Calculate(Node CurrentNode, Node GoalNode);
+    }
+
+    internal class EuclideanHuristic : IHuristic
+    {
+        public float Calculate(Node CurrentNode, Node GoalNode)
+        {
+            float dx = CurrentNode.GetX() - GoalNode.GetX();
+            float dy = CurrentNode.GetY() - GoalNode.GetY();
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+
+    public interface IPathFind
+    {
+        public List<Node> FindPath(in Graph NodeGraph, Node StartNode, Node GoalNode, IHuristic Method);
+    }
+
+    class NodeRecord
+    {
+
+    }
+
+    public class ASterPathFindSystem : IPathFind
+    {
+        public List<Node> FindPath(in Graph NodeGraph, Node StartNode, Node GoalNode, IHuristic Method)
+        {
+            List<Node> Result = new List<Node>();
+
+            return Result;
+        }
+    }
+}
--- ProjectKJServers/GameServer/Resource/MapGraph.cs
+++ ProjectKJServers/GameServer/Resource/MapGraph.cs
@@ -12,70 +12,70 @@ namespace GameServer.Resource
 {
     public class Node
     {
-        private bool CanMove;
-        private int NodeID;
-        private List<KeyValuePair<int, float>> ConnectedNodeID;
         private float X;
         private float Y;
-        // 추후 A*에서 사용할때 휴리스틱을 캐싱할 변수
-        private float Heuristic;
 
-        public Node(int NodeID, float x, float y)
+        public Node(float x, float y)
         {
-            this.NodeID = NodeID;
-            CanMove = true;
-            ConnectedNodeID = new List<KeyValuePair<int, float>>();
             X = x;
             Y = y;
-            // 휴리스틱은 -1로 초기화 현재 할당이 안되었다는 뜻
-            Heuristic = -1;
         }
-
-        public void AddConnectedNode(int NodeID, float Weight)
-        {
-            ConnectedNodeID.Add(new KeyValuePair<int, float>(NodeID, Weight));
-        }
-
-        public void RemoveConnectedNode(int NodeID)
-        {
-            ConnectedNodeID.RemoveAll(kvp => kvp.Key == NodeID);
-        }
-
-        public void SetCanMove(bool CanMove) => this.CanMove = CanMove;
-
-        public int GetNodeID() => NodeID;
-
-        public List<KeyValuePair<int, float>> GetConnectedNodes() => ConnectedNodeID;
 
         public float GetX() => X;
 
         public float GetY() => Y;
-        public float GetHeuristic() => Heuristic;
-        public void SetHeuristic(float heuristic) => Heuristic = heuristic;
+    }
+
+    public class Connection
+    {
+        private Node FromNode;
+        private Node ToNode;
+        private float Cost;
+
+        public Connection(Node FromNode, Node ToNode, float Cost)
+        {
+            this.FromNode = FromNode;
+            this.ToNode = ToNode;
+            this.Cost = Cost;
+        }
+
+        public Node GetFromNode() => FromNode;
+        public Node GetToNode() => ToNode;
+        public float GetCost() => Cost;
     }
 
 
     public class Graph
     {
-        private List<Node> Nodes;
+        private Dictionary<Node, List<Connection>> Connections;
         public Graph()
         {
-            Nodes = new List<Node>();
+            Connections = new Dictionary<Node, List<Connection>>();
         }
 
-        public void AddNode(Node Node)
+        public void AddConnection(Connection Connect)
         {
-            Nodes.Add(Node);
+            if(Connections.ContainsKey(Connect.GetFromNode()))
+            {
+                Connections[Connect.GetFromNode()].Add(Connect);
+            }
+            else
+            {
+                Connections[Connect.GetFromNode()] = new List<Connection> { Connect };
+            }
         }
 
-        public void RemoveNode(Node Node)
+        public void RemoveNode(Connection Connect)
         {
-            Nodes.Remove(Node);
+            if (Connections.ContainsKey(Connect.GetFromNode()))
+            {
+                Connections[Connect.GetFromNode()].Remove(Connect);
+            }
         }
 
-        public List<Node> GetNodes()
+        public List<Connection> GetConnections(Node FromNode)
         {
-            return Nodes;
+            return Connections[FromNode];
         }
     }
 
@@ -96,22 +96,22 @@ namespace GameServer.Resource
             float MaxY = Data.MapBoundY;
             float MinX = 0;
             float MinY = 0;
-
-            int NodeId = 0;
             Dictionary<(float, float), Node> NodeDict = new Dictionary<(float, float), Node>();
 
-            //// 디버깅용
-            //// 언리얼의 X축은 서버상의 Y축, 언리얼의 Y축은 서버상의 X축이다.
-            //// 사실 범위 체크같은거 할때는 문제 없지만, (이미 언리얼 기준축으로 변환을 다 시켜놔서)
-            //// 디버그 프린트할때 그림이 이상하게 나오면 축을 의심하면된다.
-            //string[,] map = new string[(int)MaxX+1, (int)MaxY+1];
-            //for (int i = (int)MinX; i <= (int)MaxX; i+= (int)NodeSize)
-            //{
-            //    for (int j = (int)MinY; j <= (int)MaxY; j+=(int)NodeSize)
-            //    {
-            //        map[i, j] = "-     ";
-            //    }
-            //}
+#if DEBUG
+            // 디버깅용
+            // 언리얼의 X축은 서버상의 Y축, 언리얼의 Y축은 서버상의 X축이다.
+            // 사실 범위 체크같은거 할때는 문제 없지만, (이미 언리얼 기준축으로 변환을 다 시켜놔서)
+            // 디버그 프린트할때 그림이 이상하게 나오면 축을 의심하면된다.
+            string[,] map = new string[(int)MaxX + 1, (int)MaxY + 1];
+            for (int i = (int)MinX; i <= (int)MaxX; i += (int)NodeSize)
+            {
+                for (int j = (int)MinY; j <= (int)MaxY; j += (int)NodeSize)
+                {
+                    map[i, j] = "-     ";
+                }
+            }
+#endif
 
             // 노드 생성 꼭짓점에는 부딪히지 않도록 일부 보정한다.
             for (float x = MinX + NodeSize; x <= MaxX - NodeSize; x += NodeSize)
@@ -137,8 +137,9 @@ namespace GameServer.Resource
                         if (NodeMaxX >= ObstaclesMinX && NodeMinX <= ObstaclesMaxX && NodeMaxY >= ObstaclesMinY && NodeMinY <= ObstaclesMaxY)
                         {
                             IsOverlapping = true;
-                            // 디버깅용
-                            //LogManager.GetSingletone.WriteLog($"Node ({x}, {y}) is overlapping with {Obstacles.MeshName} obstacles. Node is not created.");
+#if DEBUG
+                            LogManager.GetSingletone.WriteLog($"Node ({x}, {y}) is overlapping with {Obstacles.MeshName} obstacles. Node is not created.");
+#endif
                             break;
                         }
                     }
@@ -148,27 +149,27 @@ namespace GameServer.Resource
                         continue;
                     }
 
-                    Node NewNode = new Node(NodeId,x,y);
+                    Node NewNode = new Node(x,y);
                     NodeDict[(x, y)] = NewNode;
-                    NodeGraph.AddNode(NewNode);
-                    NodeId++;
 
-                    // 디버깅용
-                    //map[(int)x, (int)y] = "*     ";
+#if DEBUG
+                    map[(int)x, (int)y] = "*     ";
+#endif
                 }
             }
 
-            // 디버깅용
-            //for (int j = (int)MaxX; j >= (int)MinX; j--)
-            //{
-            //    string DebugString = string.Empty;
-            //    for (int i = (int)MinY; i <= (int)MaxY; i += (int)NodeSize)
-            //    {
-            //        DebugString += map[j, i];
-            //    }
-            //    if(DebugString != string.Empty)
-            //        LogManager.GetSingletone.WriteLog(DebugString);
-            //}
+#if DEBUG
+            for (int j = (int)MaxX; j >= (int)MinX; j--)
+            {
+                string DebugString = string.Empty;
+                for (int i = (int)MinY; i <= (int)MaxY; i += (int)NodeSize)
+                {
+                    DebugString += map[j, i];
+                }
+                if (DebugString != string.Empty)
+                    LogManager.GetSingletone.WriteLog(DebugString);
+            }
+#endif
 
             // 노드 연결
             foreach (var kvp in NodeDict)
@@ -179,61 +180,68 @@ namespace GameServer.Resource
                 // 오른쪽 노드 연결
                 if (NodeDict.ContainsKey((x + NodeSize, y)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x + NodeSize, y)].GetNodeID(), 1f);
+                    Connection Connect  = new Connection(CurrentNode, NodeDict[(x + NodeSize, y)], 1f);
+                    NodeGraph.AddConnection(Connect);
                 }
 
                 // 위쪽 노드 연결
                 if (NodeDict.ContainsKey((x, y + NodeSize)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x, y + NodeSize)].GetNodeID(), 1f);
+                    Connection Connect = new Connection(CurrentNode, NodeDict[(x, y + NodeSize)], 1f);
+                    NodeGraph.AddConnection(Connect);
                 }
 
                 // 왼쪽 노드 연결
                 if (NodeDict.ContainsKey((x - NodeSize, y)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x - NodeSize, y)].GetNodeID(), 1f);
+                    Connection Connect = new Connection(CurrentNode, NodeDict[(x - NodeSize, y)], 1f);
+                    NodeGraph.AddConnection(Connect);
                 }
 
                 // 아래쪽 노드 연결
                 if (NodeDict.ContainsKey((x, y - NodeSize)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x, y - NodeSize)].GetNodeID(), 1f);
+                    Connection Connect = new Connection(CurrentNode, NodeDict[(x, y - NodeSize)], 1f);
+                    NodeGraph.AddConnection(Connect);
                 }
 
                 // 오른쪽 위 대각선 노드 연결 대각선은 피타고라스 정리에 의해 루트2로 가중치를 둔다
                 if (NodeDict.ContainsKey((x + NodeSize, y + NodeSize)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x + NodeSize, y + NodeSize)].GetNodeID(), 1.414f);
+                    Connection Connect = new Connection(CurrentNode, NodeDict[(x + NodeSize, y + NodeSize)], 1.414f);
+                    NodeGraph.AddConnection(Connect);
                 }
 
                 // 오른쪽 아래 대각선 노드 연결
                 if (NodeDict.ContainsKey((x + NodeSize, y - NodeSize)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x + NodeSize, y - NodeSize)].GetNodeID(), 1.414f);
+                    Connection Connect = new Connection(CurrentNode, NodeDict[(x + NodeSize, y - NodeSize)], 1.414f);
+                    NodeGraph.AddConnection(Connect);
                 }
 
                 // 왼쪽 위 대각선 노드 연결
                 if (NodeDict.ContainsKey((x - NodeSize, y + NodeSize)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x - NodeSize, y + NodeSize)].GetNodeID(), 1.414f);
+                    Connection Connect = new Connection(CurrentNode, NodeDict[(x - NodeSize, y + NodeSize)], 1.414f);
+                    NodeGraph.AddConnection(Connect);
                 }
 
                 // 왼쪽 아래 대각선 노드 연결
                 if (NodeDict.ContainsKey((x - NodeSize, y - NodeSize)))
                 {
-                    CurrentNode.AddConnectedNode(NodeDict[(x - NodeSize, y - NodeSize)].GetNodeID(), 1.414f);
+                    Connection Connect = new Connection(CurrentNode, NodeDict[(x - NodeSize, y - NodeSize)], 1.414f);
+                    NodeGraph.AddConnection(Connect);
                 }
-
-                // 디버깅용
-                //var ConnectedNodes = CurrentNode.GetConnectedNodes();
-                //foreach (var ConnectedNodeId in ConnectedNodes)
-                //{
-                //    var ConnectedNode = NodeGraph.GetNodes().FirstOrDefault(n => n.GetNodeID() == ConnectedNodeId);
-                //    if (ConnectedNode != null)
-                //    {
-                //        LogManager.GetSingletone.WriteLog($"Node {CurrentNode.GetNodeID()} : ({CurrentNode.GetX()}, {CurrentNode.GetY()}) is connected to Node ({ConnectedNode.GetX()}, {ConnectedNode.GetY()})");
-                //    }
-                //}
+#if DEBUG
+                var ConnectedNodes = NodeGraph.GetConnections(CurrentNode);
+                foreach (var ConnectedNode in ConnectedNodes)
+                {
+                    if (ConnectedNode != null)
+                    {
+                        LogManager.GetSingletone.WriteLog($"Node ({CurrentNode.GetX()}, {CurrentNode.GetY()}) is connected to Node ({ConnectedNode.GetToNode().GetX()}, {ConnectedNode.GetToNode().GetY()})");
+                    }
+                }
+#endif
 
             }
 
