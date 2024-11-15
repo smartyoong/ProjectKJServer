f2602426ccac791a16ed8366fc3beb483cc27109
Kim Young Chan <smartyoong@naver.com>
2024-11-16 02:04:51 +0900

    데코레이터 및 병렬 노드 구현 완료 내일 테스트겸 Component에서 일일히 트리 구조 짜보고 여러가지 노드 구현해보고 테스트하고 문서화 시키자

--- ProjectKJServers/GameServer/Object/Behaviors.cs
+++ ProjectKJServers/GameServer/Object/Behaviors.cs
@@ -23,7 +23,7 @@ namespace GameServer.Object
 
         public object? GetData(string Key)
         {
-            if(DataDictionary.ContainsKey(Key))
+            if (DataDictionary.ContainsKey(Key))
             {
                 return DataDictionary[Key];
             }
@@ -64,11 +64,14 @@ namespace GameServer.Object
     public interface IParallel // 이걸 실제로 사용할 일이 있는지 모르겠네?
     {
         //Task Run을 사용하자
-        List<IBehavior> Behaviors { get; set; }
-        List<IBehavior> RunningBehavior { get; set; }
-        bool RunBehaviors();
-        bool RunChildren(CancellationToken CancleToken);
-        void Terminate(CancellationToken CancleToken);
+        List<IParallelAction> Behaviors { get; set; }
+        bool RunBehaviors(BlackBoard? Board);
+        void Terminate();
+    }
+
+    public interface IParallelAction
+    {
+        bool Run(BlackBoard? Board, CancellationToken CancleToken);
     }
 
     public class RootBehavior
@@ -167,7 +170,7 @@ namespace GameServer.Object
         }
         public bool SelectBehavior(BlackBoard? Board)
         {
-            while(true)
+            while (true)
             {
                 int Index = RandomSelector();
                 if (Behaviors[Index].Run(Board))
@@ -236,5 +239,168 @@ namespace GameServer.Object
         }
     }
 
-    // 내일 데코레이터랑 병렬 노드 구현하자
+
+    public class LimitDecorator : IBehavior, IDecorator
+    {
+        public IBehavior Behavior { get; set; }
+        private int LimitCount;
+        private int Count = 0;
+        public LimitDecorator(IBehavior Behavior, int LimitCount)
+        {
+            this.Behavior = Behavior;
+            this.LimitCount = LimitCount;
+        }
+        public bool Run(BlackBoard? Board)
+        {
+            return RunBehavior(Board);
+        }
+
+        public bool RunBehavior(BlackBoard? Board)
+        {
+            if (Count < LimitCount)
+            {
+                Count++;
+                return Behavior.Run(Board);
+            }
+            return false;
+        }
+    }
+
+    public class UntilFailDecorator : IBehavior, IDecorator
+    {
+        public IBehavior Behavior { get; set; }
+        public UntilFailDecorator(IBehavior Behavior)
+        {
+            this.Behavior = Behavior;
+        }
+        public bool Run(BlackBoard? Board)
+        {
+            return RunBehavior(Board);
+        }
+        public bool RunBehavior(BlackBoard? Board)
+        {
+            while (Behavior.Run(Board))
+            {
+                continue;
+            }
+            return true;
+        }
+    }
+
+    public class InverterDecorator : IBehavior, IDecorator
+    {
+        public IBehavior Behavior { get; set; }
+        public InverterDecorator(IBehavior Behavior)
+        {
+            this.Behavior = Behavior;
+        }
+        public bool Run(BlackBoard? Board)
+        {
+            return RunBehavior(Board);
+        }
+        public bool RunBehavior(BlackBoard? Board)
+        {
+            return !Behavior.Run(Board);
+        }
+    }
+
+    // 이걸 쓸일이 있을까?
+    public class SemaphoreDecorator : IBehavior, IDecorator
+    {
+        public IBehavior Behavior { get; set; }
+        private Semaphore Sema;
+        public SemaphoreDecorator(IBehavior Behavior, int MaxConCurrentCount)
+        {
+            this.Behavior = Behavior;
+            Sema = new Semaphore(MaxConCurrentCount, MaxConCurrentCount);
+        }
+        public bool Run(BlackBoard? Board)
+        {
+            return RunBehavior(Board);
+        }
+        public bool RunBehavior(BlackBoard? Board)
+        {
+            if (Sema.WaitOne(TimeSpan.FromSeconds(1)))
+            {
+                try
+                {
+                    return Behavior.Run(Board);
+                }
+                finally
+                {
+                    Sema.Release();
+                }
+            }
+            return false;
+        }
+    }
+
+    public class WaitAction : IAction
+    {
+        private float WaitTime;
+        public WaitAction(float WaitTime)
+        {
+            this.WaitTime = WaitTime;
+        }
+
+        public bool Run(BlackBoard? Board)
+        {
+            return Execute(Board);
+        }
+
+        public bool Execute(BlackBoard? Board)
+        {
+            Task.Delay(TimeSpan.FromSeconds(WaitTime)).Wait();
+            return true;
+        }
+    }
+    // 애초에 사용을 할까나?
+    public class ParallelBehavior : IBehavior, IParallel
+    {
+        public List<IParallelAction> Behaviors { get; set; }
+        private List<Task> RunningAction;
+        private CancellationTokenSource CancleTokenSource;
+        public ParallelBehavior()
+        {
+            Behaviors = new List<IParallelAction>();
+            CancleTokenSource = new CancellationTokenSource();
+            RunningAction = new List<Task>();
+        }
+        public bool Run(BlackBoard? Board)
+        {
+            return RunBehaviors(Board);
+        }
+        public bool RunBehaviors(BlackBoard? Board)
+        {
+            bool Result = true;
+            CancleTokenSource = new CancellationTokenSource();
+            RunningAction.Clear();
+            foreach (var Behavior in Behaviors)
+            {
+                RunningAction.Add(Task.Run(() =>
+                {
+                    if (!Behavior.Run(Board, CancleTokenSource.Token))
+                    {
+                        Interlocked.Exchange(ref Result, false);
+                        Terminate();
+                    }
+                }));
+            }
+
+            if(CancleTokenSource.Token.IsCancellationRequested)
+            {
+                return false;
+            }
+
+            Task.WaitAll(RunningAction.ToArray(),CancleTokenSource.Token);
+            return Result;
+        }
+        public void Terminate()
+        {
+            CancleTokenSource.Cancel();
+        }
+    }
+
+    // 인터럽트는 원자적으로 다른 행동노드에게 조건 변수 값을 변경 시키는건데 
+    // 구현은 가능하겠는데 이게 노드간 건너뛰는거라서 일단 구현을 배제하자 구조적 정통성이 깨진다.
 }
