333c141dd028537e70d1b7c979e01b288f3e4bc6
Kim Young Chan <smartyoong@naver.com>
2024-12-07 01:38:29 +0900

    Goap Planning에서 WorldInfo 클래스 구현완료 내가 공부한 보람이 있다 코루틴을 의미있게 사용할 수 있는 방법을 발견했다 DFS(모든 조합을 구할때)랑 콜백이다

--- ProjectKJServers/GameServer/Object/GOAP.cs
+++ ProjectKJServers/GameServer/Object/GOAP.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,47 @@ namespace GameServer.Object
             this.Value += Value;
         }
     }
+
+
+    public class GOAP
+    {
+        private List<GOAPGoal> Goals;
+        private List<IGOAPAction> Actions;
+        public GOAP(List<GOAPGoal> Goals, List<IGOAPAction> Actions)
+        {
+            this.Goals = Goals;
+            this.Actions = Actions;
+        }
+
+        public float CalculateDiscontentment()
+        {
+            return Goals.Aggregate(0f, (discontentment, goal) => discontentment + goal.GetDiscontentment(goal.Value));
+        }
+
+        public void ApplyAction(IGOAPAction Action)
+        {
+            foreach (var Goal in Goals)
+            {
+                Goal.AddValue(Action.GetGoalChange(Goal));
+            }
+        }
+        // 재귀 형식으로 사용하면서, 현재의 함수 진행상황을 일시정지했다가 재개했다가 하는 형식이므로
+        // 코루틴이 매우 적합하다.
+        // C#의 코루틴은 IEnumerator를 통해 외부에서 관리하기때문에, 매개변수로 이 클래스를 넘겨도 문제가 없다.
+        // 그러나 C++은 co_yield, co_return을 사용하여서 내부에서 관리하기 때문에 매개변수로 넘겨도 상태가 저장된다. co객체에서
+        public IEnumerator<IGOAPAction> GetActionCoroutine()
+        {
+            for (int i = 0; i < Actions.Count; i++)
+            {
+                yield return Actions[i];
+            }
+        }
+        public int GetMaxDepth()
+        {
+            return Actions.Count;
+        }
+    }
+
 
     public class TestGOAPAction : IGOAPAction
     {
